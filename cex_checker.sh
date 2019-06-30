1c7f897a77b6124d65d4fcb148d8c2a6e63232ec
Bonddy1986 <50744907+Bonddy1986@users.noreply.github.com>
2019-06-30 01:10:49 +0100

    Update cex_checker.sh

--- cex_checker.sh
+++ cex_checker.sh
@@ -2,11 +2,12 @@
 
 #product_code=045496510060 #3DS Circle Pad Pro
 product_code=045496510107 #3DS XL Circle Pad Pro
-
+product_code=497436561310 #shining in the darkness
+product_code=045496430450 #Mega Man 4, Unboxed
 if [[ -f product.html ]]; then
     rm -f product.html
 fi
-
+while true; do ./cex_checker.sh && sleep 300; done
 curl -s -L -o product.html https://uk.webuy.com/product-detail?id=${product_code}
 
 if [[ -n $(grep "In stock online" product.html) ]]; then
